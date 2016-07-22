273d1ec6eec78cca7af1e11dd6e567da32e467bd
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-07-22 14:27:40 -0500

    deleted line of code

--- quiz.js
+++ quiz.js
@@ -2,7 +2,6 @@
 
 document.getElementById('button').addEventListener('click',tree);
 var div = document.getElementById('placeForTree');
-var myContent = document.createElement('content');
 
 function tree(height) {
   height = document.getElementById('myHeight').value;
@@ -14,7 +13,7 @@ function tree(height) {
     line =' '.repeat(spaceNumbers) + character.repeat(addCounter);
     spaceNumbers -=1
     addCounter += 2
-    div.innerHTML += line+ "<br>";
+    div.innerHTML += line + "<br>";
 
     }
 
