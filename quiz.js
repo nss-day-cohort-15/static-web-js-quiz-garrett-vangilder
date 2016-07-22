8dedffc75756fe9d2f05aeb2489a816bbd70cb9d
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-07-22 14:14:04 -0500

    fixed function

--- quiz.js
+++ quiz.js
@@ -1,3 +1,22 @@
-function tree (height) {
 
-}
+
+document.getElementById('button').addEventListener('click',tree);
+var div = document.getElementById('placeForTree');
+var myContent = document.createElement('content');
+
+function tree(height) {
+  height = document.getElementById('myHeight').value;
+  character = document.getElementById('myCharacter').value;
+  addCounter = 1;
+  spaceNumbers = height
+
+  for (i = 0; i< height; i ++) {
+    line =' '.repeat(spaceNumbers) + character.repeat(addCounter);
+    spaceNumbers -=1
+    addCounter += 2
+    div.innerHTML += line+ "<br>";
+
+    }
+
+  }
+
