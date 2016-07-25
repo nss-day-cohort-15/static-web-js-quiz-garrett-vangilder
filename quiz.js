a4efaa19a78387a6904246ce23a180f0a43051ad
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-07-25 13:50:35 -0500

    added a few features such as warnings should an improper amount be entered. Split the original tree function into two separate functions."

--- quiz.js
+++ quiz.js
@@ -1,22 +1,46 @@
+document.getElementById('button').addEventListener('click',createCharacters);
+// document.getElementById('myHeight').addEventListener('keypress',function (e) {
+//     while (keyCode === 13) {
+//       createCharacters();
+//     }
+// });
+// document.getElementById('characterOfTree').addEventListener('keypress',function (e) {
+//     while (keyCode === 13) {
+//       createCharacters();
+//     }
+// });
 
-
-document.getElementById('button').addEventListener('click',tree);
 var div = document.getElementById('placeForTree');
+var obj = {}
 
-function tree(height) {
-  height = document.getElementById('myHeight').value;
-  character = document.getElementById('myCharacter').value;
+function createCharacters () {
+      console.log('createCharacters function has began')
+      obj.height = Number(document.getElementById('myHeight').value);
+      obj.character = document.getElementById('myCharacter').value;
+      tree(obj);
+    }
+
+function tree(obj) {
+  console.log("Tree Function is Running")
+
   addCounter = 1;
-  spaceNumbers = height
+  spaceNumbers = obj.height;
+    console.log('Loops are starting')
+  if ( obj.height < 0)
+    alert('You need to enter an amount above 0!');
+  if (obj.height =="")
+      alert("You left the enter field empty. Please enter in a height and character.");
 
-  for (i = 0; i< height; i ++) {
-    line =' '.repeat(spaceNumbers) + character.repeat(addCounter);
+  else if (obj.height > 0) {
+  for (i = 0; i< obj.height; i ++) {
+    line =' '.repeat(spaceNumbers) + obj.character.repeat(addCounter);
     spaceNumbers -=1
     addCounter += 2
     div.innerHTML += line + "<br>";
     console.log(line);
-
     }
-
   }
+  }
+
+
 
